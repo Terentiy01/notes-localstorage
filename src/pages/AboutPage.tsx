d56aba9ed27e1e7d404636496ede47d6cd642855
Almaz <almazius11@gmail.com>
2023-02-04 14:51:31 +0300

    created TodosPage and AboutPage

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutPage: React.FC = () => {
+  return (
+    <>
+      <h1>Страница информации</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam officiis
+        praesentium repellendus nulla. Reiciendis earum saepe officiis provident
+        officia alias.
+      </p>
+      <button className="btn">Обратно к списку дел</button>
+    </>
+  )
+}
+
+export default AboutPage
